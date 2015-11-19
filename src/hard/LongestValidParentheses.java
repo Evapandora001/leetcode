a57664f14c849f4fa200477ca4c3934d5c7b12be
Evapandora001 <evapandora@sina.cn>
2015-11-19 11:19:44 +0800

    2015-11-19-Time Limit Exceeded

--- src/hard/LongestValidParentheses.java
+++ src/hard/LongestValidParentheses.java
@@ -2,13 +2,43 @@ package hard;
 
 public class LongestValidParentheses {
     public int longestValidParentheses(String s) {
-        int len=0;
-        return len;
+    	if(s.length()==0) return 0;
+    	int start=0,end=s.length()-1;
+    	while(start<s.length()&&s.charAt(start)!='(') start++;
+    	while(end>=0&&s.charAt(end)!=')') end--;
+    	if(end<=start) return 0;
+    	int maxLen=0;
+    	for(int i=start;i<=end;i++){
+    		if(s.charAt(i)!='(') continue;
+    		int len=0,lCount=0,rCount=0;
+    		for(int j=i;j<=end;j++){
+        		char ele=s.charAt(j);
+        		if(ele=='('){
+        			lCount++;
+        		}else{
+        			if(rCount<lCount){
+        				rCount++;
+        				if(lCount==rCount){
+        					len=lCount*2;
+        				}
+        			}else{
+        				if(len>maxLen) maxLen=len;
+        				break;
+        			}
+        		}
+    		}
+    		if(len>maxLen) maxLen=len;
+    	}
+        return maxLen;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LongestValidParentheses a=new LongestValidParentheses();
+//		System.out.println(a.longestValidParentheses(")()())"));
+//		System.out.println(a.longestValidParentheses("))())(((()))"));
+		System.out.println(a.longestValidParentheses(""));
+//		System.out.println(a.longestValidParentheses("("));
+//		System.out.println(a.longestValidParentheses(")"));
 	}
 
 }
