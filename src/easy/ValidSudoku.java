0ff0c01f301d6efd7c4f83faf367373f221cbb92
Evapandora001 <evapandora@sina.cn>
2015-11-23 21:09:35 +0800

    2015-11-23

--- src/easy/ValidSudoku.java
+++ src/easy/ValidSudoku.java
@@ -13,7 +13,7 @@ public class ValidSudoku {
 	//'.' means empty
     public boolean isValidSudoku(char[][] board) {
     	if(board==null||board.length!=9||board[0].length!=9) return false;
-    	boolean[][] arrow=new boolean[9][9];
+    	boolean[][] row=new boolean[9][9];
     	boolean[][] column=new boolean[9][9];
     	boolean[][] grids=new boolean[9][9];
     	for(int i=0;i<9;i++){
@@ -21,10 +21,10 @@ public class ValidSudoku {
     			char key=board[i][j];
     			if(key=='.') continue;
     			int grid=i/3*3+j/3;
-    			if(arrow[key-'1'][i]||column[key-'1'][j]||grids[key-'1'][grid]){//由于个数少，通过位置判断 是否存在，而不需要O(n)查找
+    			if(row[key-'1'][i]||column[key-'1'][j]||grids[key-'1'][grid]){//由于个数少，通过位置判断 是否存在，而不需要O(n)查找
     				return false;
     			}else{
-    				arrow[key-'1'][i]=true;
+    				row[key-'1'][i]=true;
     				column[key-'1'][j]=true;
     				grids[key-'1'][grid]=true;
     			}
--- src/hard/SudokuSolver.java
+++ src/hard/SudokuSolver.java
@@ -1,13 +1,60 @@
 package hard;
 
+import java.util.Arrays;
+
 public class SudokuSolver {
     public void solveSudoku(char[][] board) {
-        
+    	int count=0;
+    	boolean[][] row=new boolean[9][9];
+    	boolean[][] column=new boolean[9][9];
+    	boolean[][] grids=new boolean[9][9];
+    	for(int i=0;i<9;i++){
+    		for(int j=0;j<9;j++){
+    			char key=board[i][j];
+    			if(key=='.'){
+    				count++;
+    				continue;
+    			}
+    			int grid=i/3*3+j/3;
+    			row[key-'1'][i]=true;
+    			column[key-'1'][j]=true;
+    			grids[key-'1'][grid]=true;
+    		}
+    	}
+    	sudoku(row,column,grids,count,board);
     }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void sudoku(boolean[][] row, boolean[][] column, boolean[][] grids, int count, char[][] board) {
+		if(count==0) return;
+		for(int num=0;num<9;num++){
+			for(int i=0;i<9;i++){
+				if(row[num][i]==true) continue;
+				for(int j=0;j<9;j++){
+					if(column[num][j]==true) continue;
+					int grid=i/3*3+j/3;
+					if(grids[num][grid]==true) continue;
+					//get a valid insert-position
+					row[num][i]=true;
+					column[num][j]=true;
+					grids[num][grid]=true;
+					board[i][j]=(char) (num+'1');
+					System.out.println(i+"#"+j+"#"+board[i][j]);
+					count--;
+					sudoku(row, column, grids, count, board);
+				}
+			}
+		}
+	}
 
+	public static void main(String[] args) {
+		SudokuSolver a=new SudokuSolver();
+		char[][] board=new char[][]{".9748....".toCharArray(),"7........".toCharArray(),".2.1.9...".toCharArray(),
+			"..7...24.".toCharArray(),".64.1.59.".toCharArray(),".98...3..".toCharArray(),
+			"...8.3.2.".toCharArray(),"........6".toCharArray(),"...2759..".toCharArray()};
+			a.solveSudoku(board);
+			for(int i=0;i<9;i++){
+				System.out.println(Arrays.toString(board[i]));
+			}
 	}
 
 }
