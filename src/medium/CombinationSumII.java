55fc7b1096b6862fc743b1708aed6f5b53f11852
Evapandora001 <evapandora@sina.cn>
2015-11-23 17:19:30 +0800

    2015-11-23

--- src/medium/CombinationSumII.java
+++ src/medium/CombinationSumII.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class CombinationSum {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+public class CombinationSumII {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
     	Arrays.sort(candidates);
         List<List<Integer>> res=new ArrayList<List<Integer>>();
     	if(candidates==null||target<candidates[0]) return res;
@@ -13,24 +13,23 @@ public class CombinationSum {
         cSum(candidates,0, res, target, ele);//start 去重 保证ele 是增序填充元素
         return res;
     }
-
+    
 	public void cSum(int[] candidates,int start,List<List<Integer>> res, int target, ArrayList<Integer> ele) {
 		if(target<0) return;
 		if(target==0){
 			res.add(new ArrayList<Integer>(ele));
 			return;
 		}
-		for(;start<candidates.length;start++){
-			if(start>0&&candidates[start]==candidates[start-1]) continue;
-			ele.add(candidates[start]);
-			cSum(candidates,start,res,target-candidates[start],ele);
+		for(int i=start;i<candidates.length;i++){
+			if(i>start&&candidates[i]==candidates[i-1]) continue;
+			ele.add(candidates[i]);
+			cSum(candidates,i+1,res,target-candidates[i],ele);
 			ele.remove(ele.size()-1);//remove 最近一次add的元素
 		}
 	}
-
 	public static void main(String[] args) {
-		CombinationSum a=new CombinationSum();
-		for (List<Integer> ele:a.combinationSum(new int[]{7,3,9,6},6)) {
+		CombinationSumII a=new CombinationSumII();
+		for (List<Integer> ele:a.combinationSum2(new int[]{10,1,2,7,6,1,5},8)) {
 			System.out.println(ele.toString());
 		}
 	}
