51a1e42e2d7644fd1fd440ec63e28208da4111b9
Evapandora001 <evapandora@sina.cn>
2015-11-23 13:05:31 +0800

    2015-11-23
    to be solved

---
+++ src/hard/SudokuSolver.java
@@ -0,0 +1,13 @@
+package hard;
+
+public class SudokuSolver {
+    public void solveSudoku(char[][] board) {
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
