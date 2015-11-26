8188bd9272fabd367eeff55012879655eeb2e86b
Evapandora001 <evapandora@sina.cn>
2015-11-26 17:36:19 +0800

    2015-11-26

--- src/hard/FirstMissingPositive.java
+++ src/hard/FirstMissingPositive.java
@@ -6,11 +6,13 @@ public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
     	if(nums==null||nums.length==0) return 1;
     	for(int i=0;i<nums.length;i++){
-    		if(nums[i]<=0||nums[i]-1>=nums.length) continue;
-    		if(nums[i]==i+1) continue;
+//    		key: put the num[i] into the right position  
+//    		nums[i]==nums[nums[i]-1 means the position to be swapped is already the right position so need not swap
+    		if(nums[i]<=0||nums[i]>=nums.length||nums[i]==i+1||nums[i]==nums[nums[i]-1]) continue;
     		int temp=nums[nums[i]-1];
     		nums[nums[i]-1]=nums[i];
     		nums[i]=temp;
+    		i--;
     	}
     	System.out.println(Arrays.toString(nums));
     	for(int i=0;i<nums.length;i++){
@@ -24,7 +26,7 @@ public class FirstMissingPositive {
 	public static void main(String[] args) {
 		FirstMissingPositive a=new FirstMissingPositive();
 		System.out.println(a.firstMissingPositive(new int[]{3,4,-1,1,1,3}));
-		System.out.println(a.firstMissingPositive(new int[]{8,6,5,4,3,2,1}));
+		System.out.println(a.firstMissingPositive(new int[]{7,6,5,4,3,2,1}));
 		System.out.println(a.firstMissingPositive(new int[]{2}));
 		System.out.println(a.firstMissingPositive(new int[]{3,4,-1,1}));
 	}
---
+++ src/hard/TrappingRainWater.java
@@ -0,0 +1,10 @@
+package hard;
+
+public class TrappingRainWater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
