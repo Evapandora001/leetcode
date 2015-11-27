222d5670a9af280c9078341f8e0d768de173a31d
Evapandora001 <evapandora@sina.cn>
2015-11-27 20:20:38 +0800

    2015-11-27

---
+++ src/medium/Permutations.java
@@ -0,0 +1,14 @@
+package medium;
+
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<LinkedList<Integer>> res
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
