fd2fbfdc8feeabf70a3fee0b0d2b2e7f4da0459a
Evapandora001 <evapandora@sina.cn>
2015-12-08 20:44:00 +0800

    2015-12-08

--- src/hard/TextJustification.java
+++ src/hard/TextJustification.java
@@ -26,9 +26,9 @@ public class TextJustification {
     		if(words[i].trim().equals("")){
     			continue;
     		}
-    		
     	}
         return res;
+        
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
