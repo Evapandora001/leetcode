c89cced7d62635477f2753316a9b15ccb60c6c45
Evapandora001 <evapandora@sina.cn>
2016-09-06 20:06:15 +0800

    2016-09-06

--- src/didi/Main.java
+++ src/didi/Main.java
@@ -1,20 +1,38 @@
-/**  
- * @Title: Main.java
- * @Prject: leetcode
- * @Package: wangyi
- * @Description: TODO
- * @author: Evapandora  
- * @date: 2016年8月2日 下午7:40:29  
- */
 package didi;
 
 import java.util.Scanner;
 
+//连续最大和
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("wanzong");
+		int N = Integer.parseInt(in.nextLine());
+		long pre = 0, max = 0;
+		for (int i = 0; i < N; i++) {
+			int ele = in.nextInt();
+			if (i == 0) {
+				pre = ele;
+				max = ele;
+				continue;
+			}
+			if (ele <= 0) {
+				if (pre <= 0) {
+					pre = ele;
+				} else {
+					pre = pre + ele;
+				}
+			} else {
+				if (pre <= 0) {
+					pre = ele;
+				} else {
+					pre = pre + ele;
+				}
+			}
+			max = Math.max(max, pre);
+
+		}
+		System.out.println(max);
 	}
 
 }
